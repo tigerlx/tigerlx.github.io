f51cc822b13d8203335b82294429aaf94af0a346
李小虎 <lixiaohu@lixiaohudeMacBook-Pro.local>
2018-12-28 10:12:20 +0800

    aaa

--- my-blog/vue.config.js
+++ my-blog/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     // 基本路径
-    baseUrl: "/",
+    baseUrl: '/',
     // 输出文件目录
-    outputDir: "dist",
+    outputDir: 'dist',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
     // use the full build with in-browser compiler?
@@ -27,21 +27,17 @@ module.exports = {
     },
     // use thread-loader for babel & TS in production build
     // enabled by default if the machine has more than 1 cores
-    parallel: require("os").cpus().length > 1,
+    parallel: require('os').cpus().length > 1,
     // PWA 插件相关配置
     // see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
     pwa: {},
     // webpack-dev-server 相关配置
     devServer: {
-        open: process.platform === "darwin",
-        host: "0.0.0.0",
         port: 8080,
-        https: false,
-        hotOnly: false,
         proxy: null, // 设置代理
         before: app => {}
     },
     // 第三方插件配置
     pluginOptions: {
     }
-};
+}
